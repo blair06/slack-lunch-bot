ef08bb269839efac329dc007f8df6878ad195a05
김혜원 <hyewon980610@gmail.com>
2021-07-15 03:00:15 +0900

    수정

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,32 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.post('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+  let body = req.body;
+    let event = body.event;
+    if (body.type === "event_callback") {
+      console.log(event);
+      if (event.type === "message") {
+        if (event.text === "안녕") {
+          console.log(
+            `메시지 수신 channel:${event.channel}, user:${event.user}`
+          );
+          web.chat
+            .postMessage({ channel: event.channel, text: "안녕하세요 :wink:" })
+            .then((result) => {
+              console.log("Message sent: " + result.ts);
+            });
+          res.sendStatus(200);
+        }
+      }
+    } else if (body.type === "url_verification") {
+      // URL 검증을 위한 처리
+      console.log("url verification");
+      res.send(body.challenge);
+    } else {
+      res.sendStatus(200);
+    }
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,39 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-const { WebClient } = require("@slack/web-api");
-
-// Read a token from the environment variables
-// const token = process.env.SLACK_TOKEN;
-const token = 'xoxb-2297933827824-2280457853732-XWOio2xG6RfQGY4WiPzWpxIQ';
-
-
-// Initialize
-const web = new WebClient(token);
-
-router.post('/slack/events',(ctx)=>{
-  const body = ctx.request.body;
-    const event = body.event;
-
-    if(body.type === 'event_callback'){
-        //메세지 이벤트
-        if(event.text === '가져와'){
-            console.log(`수신 channel:${event.channel}, user: ${event.user}`);
-            web.chat.postMessage({
-                channel: event.channel,
-                text: '가져올게요'
-            }).then(result => {
-                console.log('메세지 발신 : ' + result.ts);
-            });
-            ctx.status = 200;
-        }
-    } else if(body.type === 'url_verification'){
-        //url 검증
-        console.log('url verification');
-        ctx.body = body.challenge;
-    } else {
-        ctx.status = 200;
-    }
-})
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
 
 module.exports = router;
