4b4ac6acea591981e93af9ec38a8510fcef36cf7
김혜원 <hyewon980610@gmail.com>
2021-07-18 16:24:38 +0900

    fixed untracked files

--- config/key.js
+++
@@ -1,8 +0,0 @@
-const token = "xoxb-2297933827824-2280457853732-XWOio2xG6RfQGY4WiPzWpxIQ";
-
-const ClientID = "35Jn3j2zkfPSLDbv2C4l";
-const ClientSecret = "BYHHYSgYAu";
-
-exports.token = token;
-exports.ClientID = ClientID;
-exports.ClientSecret = ClientSecret;
