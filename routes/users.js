3afb4d449eee0942f8a63d64909b2b74a0d70176
김혜원 <hyewon980610@gmail.com>
2021-07-18 16:03:05 +0900

    test

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const crawling = require("../lib/crawling");
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  const restaurants = crawling.getRestaurants("노원구 맛집");
+  const restaurants = crawling.getRestaurants("영등포구 맛집");
   restaurants.then((response) => {
     res.send(response.title);
   });
